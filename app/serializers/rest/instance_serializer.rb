1ffe595ba7f158a62953dd936b17b643fb93574a
Nicolas Dextraze <nicolas.dextraze@adaptechgroup.com>
2024-10-12 09:51:37 -0400

    fix max chars to 1024

--- app/serializers/rest/instance_serializer.rb
+++ app/serializers/rest/instance_serializer.rb
@@ -11,8 +11,8 @@ class REST::InstanceSerializer < ActiveModel::Serializer
   include RoutingHelper
 
   attributes :domain, :title, :version, :source_url, :description,
-             :usage, :thumbnail, :icon, :languages, :configuration,
-             :registrations, :api_versions
+             :usage, :thumbnail, :languages, :configuration,
+             :registrations, :max_toot_chars
 
   has_one :contact, serializer: ContactSerializer
   has_many :rules, serializer: REST::RuleSerializer
@@ -109,6 +109,10 @@ class REST::InstanceSerializer < ActiveModel::Serializer
 
   def api_versions
     Mastodon::Version.api_versions
+  end
+
+  def max_toot_chars
+    StatusLengthValidator::MAX_CHARS
   end
 
   private
